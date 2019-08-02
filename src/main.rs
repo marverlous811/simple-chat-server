1399ce294a2e2f8ca6139d3ca610a60e46021540
hieunq <hieunq@ows.vn>
2019-08-02 17:47:50 +0700

    create server handler

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,53 @@ extern crate ws;
 use std::env;
 use std::error::Error;
 use std::process;
-use ws::{listen, Handler, Sender, Handshake, Message};
+use ws::{listen, Handler, Sender, Handshake, Message, Factory};
+
+struct WsHandler {
+    ws: Sender,
+    isClient: bool
+}
+
+impl Handler for WsHandler {
+    fn on_open(&mut self, handShake: Handshake) -> ws::Result<()>{
+        if self.isClient {
+            println!("Server have a connection");
+        }
+        else {
+            println!("Client connected");
+        }
+
+        Ok(())
+    }
+}
+
+struct Server{
+}
+
+impl Default for Server {
+    fn default() -> Self {
+        Server {}
+    }
+}
+
+impl Factory for Server {
+    type Handler = WsHandler;
+
+    fn connection_made(&mut self, ws: Sender) -> WsHandler {
+        WsHandler {
+            ws: ws,
+            isClient: false
+        }
+    }
+
+    fn client_connected(&mut self, ws: Sender) -> WsHandler {
+        println!("client connected");
+        WsHandler {
+            ws: ws,
+            isClient: true
+        }
+    }
+}
 
 fn getAddr(mut args: std::env::Args) -> Result<String, &'static str>{
     args.next();
@@ -27,9 +73,7 @@ fn main() {
 
     println!("address: {}", addr);
 
-    listen(addr, |out| {
-        move |msg| {
-            out.send(msg)
-        }
-    }).unwrap();
+    let ws = ws::WebSocket::new(Server::default()).expect("cannot create server");
+    ws.listen(addr).unwrap();
+
 }
